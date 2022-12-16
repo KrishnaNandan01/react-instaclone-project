e101c649ffada2f8facc8a58366181df2aa8644f
krishnanandan <nandan31138@gmail.com>
2022-12-16 21:30:22 +0530

    react files added

---
+++ instaclone/src/Component/landingPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import image from "../images/image_landing.jpg"
+import "../Styles/App.css";
+import { Link } from "react-router-dom";
+const LandingPage = ()=>{
+
+    return(<div>
+        <div>
+           <h1>Landing Page</h1>
+        </div>
+
+        <div>
+            <span >
+               <img  className="container" src={image} alt="landing"/>
+            </span>
+            <span className="rihtPart">
+               <strong >10x Team 04</strong>
+                
+            </span>
+            <span id="btn">
+                <Link to="/abc"> <button >Enter</button> </Link>
+               
+            </span>
+        </div>
+    </div>)
+}
+export default LandingPage;
